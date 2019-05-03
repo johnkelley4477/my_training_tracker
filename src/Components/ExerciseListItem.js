65e107a81960709f739457e0d89462f387a43a26
John Kelley <john.r.kelley@lowes.com>
2019-05-03 13:20:37 -0400

    Convert ExcerciseListItem to hooks

--- src/Components/ExerciseListItem.js
+++ src/Components/ExerciseListItem.js
@@ -2,16 +2,9 @@
 import React from 'react';
 import {Link} from '@reach/router';
 
-class exerciseListItem extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			exercise: '',
-			lastDate: ''
-		}
-	}
-	daysFromToday(){
-		const pasted = new Date(this.props.date);
+function exerciseListItem(props){
+	function daysFromToday(){
+		const pasted = new Date(props.date);
 		const today = new Date();
 		const days = Math.abs(today - pasted);
 		const count =  Math.floor(days / (1000*60*60*24));
@@ -22,18 +15,16 @@ class exerciseListItem extends React.Component{
 		}
 		return `Last performed ${count} day ago`;
 	}
-	render(){
-		return(
-			<Link to= {"/" + this.props.exercise.toLowerCase()} className="no_dec text_center">
-				<div>
-					<h2 className="font_large">{this.props.exercise}</h2>
-				</div>
-				<div>
-					<span>{this.daysFromToday()}</span>
-				</div>
-			</Link>
-		);
-	}
+	return(
+		<Link to= {"/" + props.exercise.toLowerCase()} className="no_dec text_center">
+			<div>
+				<h2 className="font_large">{props.exercise}</h2>
+			</div>
+			<div>
+				<span>{daysFromToday()}</span>
+			</div>
+		</Link>
+	)
 }
 
 export default exerciseListItem;
