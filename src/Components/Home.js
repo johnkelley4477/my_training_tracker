d82d747336d4abc17288ce6e77940dff942920df
John Kelley <john.r.kelley@lowes.com>
2019-05-03 12:54:35 -0400

    Convert home component to hooks

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,19 +2,16 @@
 import React from 'react';
 /* Components */
 import Exercise from './ExerciseListItem';
-class Home extends React.Component {
-  render(){
-    return(
-
-      <div>
-          <Exercise date={this.props.last.Pull} exercise="Pull" />
-          <Exercise date={this.props.last.Push} exercise="Push" />
-          <Exercise date={this.props.last.Abs} exercise="Abs" />
-          <Exercise date={this.props.last.Legs} exercise="Legs" />
-          <Exercise date={this.props.last.Spartan} exercise="Spartan" />
-      </div>
-    )
-  }
+function Home (props) {
+  return(
+    <div>
+        <Exercise date={props.last.Pull} exercise="Pull" />
+        <Exercise date={props.last.Push} exercise="Push" />
+        <Exercise date={props.last.Abs} exercise="Abs" />
+        <Exercise date={props.last.Legs} exercise="Legs" />
+        <Exercise date={props.last.Spartan} exercise="Spartan" />
+    </div>
+  )
 }
 
 export default Home;
